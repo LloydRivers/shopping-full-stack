3810350a94d38b6957f866ee75f79f986cc2147e
LloydRivers <lloydrivers58@hotmail.com>
2022-05-13 08:26:36 +0100

    added mongo db

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(
+  "mongodb://localhost:27017/mern-app",
+  { useNewUrlParser: true },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connection Successful");
+    }
+  }
+);
 
 // Importing Controllers
 const productController = require("./controllers/ProductController");
