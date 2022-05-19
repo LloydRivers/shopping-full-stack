075784c9c34be57be8b109fb7cf998a93edcf0fa
LloydRivers <lloydrivers58@hotmail.com>
2022-05-19 07:54:38 +0100

    add dynamic vales to the

--- client/src/components/card/CartCard.js
+++ client/src/components/card/CartCard.js
@@ -1,43 +1,29 @@
 import React from "react";
 
-function CartCard() {
+function CartCard({ item }) {
+  console.log(item);
   return (
     <>
       <div className="row mb-4 d-flex justify-content-between align-items-center">
         <div className="col-md-2 col-lg-2 col-xl-2">
           <img
-            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img5.webp"
+            src={item.image}
             className="img-fluid rounded-3"
             alt="Cotton T-shirt"
           />
         </div>
         <div className="col-md-3 col-lg-3 col-xl-3">
-          <h6 className="text-muted">Shirt</h6>
-          <h6 className="text-black mb-0">Cotton T-shirt</h6>
+          <h5 className="text-black mb-0">{item.title}</h5>
         </div>
         <div className="col-md-3 col-lg-3 col-xl-2 d-flex">
-          <button
-            className="btn btn-link px-2"
-            onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
-          >
-            <i className="fas fa-minus"></i>
-          </button>
-
           <input
             id="form1"
             min="0"
             name="quantity"
-            value="1"
+            value={item.quantity}
             type="number"
             className="form-control form-control-sm"
           />
-
-          <button
-            className="btn btn-link px-2"
-            onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
-          >
-            <i className="fas fa-plus"></i>
-          </button>
         </div>
         <div className="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
           <h6 className="mb-0"></h6>
