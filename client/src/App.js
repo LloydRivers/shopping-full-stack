3c17bc6ddbca5f1afad5aedab21774b84d850eeb
LloydRivers <lloydrivers58@hotmail.com>
2022-05-19 07:53:48 +0100

    import context api...
    removed redundant bootstrap.
    added total products in the cart
    render card

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Container } from "react-bootstrap";
 import AllRoutes from "./config/routing/AllRoutes";
 
+export const CartContext = React.createContext();
 function App() {
+  const [cartItems, setCartItems] = useState([]);
   return (
     <Container>
-      <AllRoutes />
+      <CartContext.Provider value={[cartItems, setCartItems]}>
+        <AllRoutes />
+      </CartContext.Provider>
     </Container>
   );
 }
