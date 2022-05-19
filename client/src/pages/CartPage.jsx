a0c68ba455d62ff941cbbda5523625ee64d0ea47
LloydRivers <lloydrivers58@hotmail.com>
2022-05-19 07:55:44 +0100

    remove redundant bootstrap

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,8 +1,12 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { CartContext } from "../App";
 import "../styling/cart-page.css";
 import CartCard from "../components/card/CartCard";
 
 function CartPage() {
+  const [cartItems] = useContext(CartContext);
+
   return (
     <>
       <section className="h-custom">
@@ -21,160 +25,23 @@ function CartPage() {
                           <h1 className="fw-bold mb-0 text-black">
                             Shopping Cart
                           </h1>
-                          <h6 className="mb-0 text-muted">3 items</h6>
+                          <h6 className="mb-0 text-muted">
+                            {cartItems.length} items
+                          </h6>
                         </div>
                         <hr className="my-4" />
-                        <CartCard />
-
-                        {/* <div className="row mb-4 d-flex justify-content-between align-items-center">
-                          <div className="col-md-2 col-lg-2 col-xl-2">
-                            <img
-                              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img5.webp"
-                              className="img-fluid rounded-3"
-                              alt="Cotton T-shirt"
-                            />
-                          </div>
-                          <div className="col-md-3 col-lg-3 col-xl-3">
-                            <h6 className="text-muted">Shirt</h6>
-                            <h6 className="text-black mb-0">Cotton T-shirt</h6>
-                          </div>
-                          <div className="col-md-3 col-lg-3 col-xl-2 d-flex">
-                            <button
-                              className="btn btn-link px-2"
-                              onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
-                            >
-                              <i className="fas fa-minus"></i>
-                            </button>
-
-                            <input
-                              id="form1"
-                              min="0"
-                              name="quantity"
-                              value="1"
-                              type="number"
-                              className="form-control form-control-sm"
-                            />
-
-                            <button
-                              className="btn btn-link px-2"
-                              onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
-                            >
-                              <i className="fas fa-plus"></i>
-                            </button>
-                          </div>
-                          <div className="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                            <h6 className="mb-0">€ 44.00</h6>
-                          </div>
-                          <div className="col-md-1 col-lg-1 col-xl-1 text-end">
-                            <a href="#!" className="text-muted">
-                              <i className="fas fa-times"></i>
-                            </a>
-                          </div>
-                        </div>
-                        <hr className="my-4" /> */}
-
-                        {/* <div className="row mb-4 d-flex justify-content-between align-items-center">
-                          <div className="col-md-2 col-lg-2 col-xl-2">
-                            <img
-                              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img6.webp"
-                              className="img-fluid rounded-3"
-                              alt="Cotton T-shirt"
-                            />
-                          </div>
-                          <div className="col-md-3 col-lg-3 col-xl-3">
-                            <h6 className="text-muted">Shirt</h6>
-                            <h6 className="text-black mb-0">Cotton T-shirt</h6>
-                          </div>
-                          <div className="col-md-3 col-lg-3 col-xl-2 d-flex">
-                            <button
-                              className="btn btn-link px-2"
-                              onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
-                            >
-                              <i className="fas fa-minus"></i>
-                            </button>
-
-                            <input
-                              id="form1"
-                              min="0"
-                              name="quantity"
-                              value="1"
-                              type="number"
-                              className="form-control form-control-sm"
-                            />
-
-                            <button
-                              className="btn btn-link px-2"
-                              onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
-                            >
-                              <i className="fas fa-plus"></i>
-                            </button>
-                          </div>
-                          <div className="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                            <h6 className="mb-0">€ 44.00</h6>
-                          </div>
-                          <div className="col-md-1 col-lg-1 col-xl-1 text-end">
-                            <a href="#!" className="text-muted">
-                              <i className="fas fa-times"></i>
-                            </a>
-                          </div>
-                        </div> */}
-
-                        {/* <hr className="my-4" /> */}
-                        {/* 
-                        <div className="row mb-4 d-flex justify-content-between align-items-center">
-                          <div className="col-md-2 col-lg-2 col-xl-2">
-                            <img
-                              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img7.webp"
-                              className="img-fluid rounded-3"
-                              alt="Cotton T-shirt"
-                            />
-                          </div>
-                          <div className="col-md-3 col-lg-3 col-xl-3">
-                            <h6 className="text-muted">Shirt</h6>
-                            <h6 className="text-black mb-0">Cotton T-shirt</h6>
-                          </div>
-                          <div className="col-md-3 col-lg-3 col-xl-2 d-flex">
-                            <button
-                              className="btn btn-link px-2"
-                              onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
-                            >
-                              <i className="fas fa-minus"></i>
-                            </button>
-
-                            <input
-                              id="form1"
-                              min="0"
-                              name="quantity"
-                              value="1"
-                              type="number"
-                              className="form-control form-control-sm"
-                            />
-
-                            <button
-                              className="btn btn-link px-2"
-                              onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
-                            >
-                              <i className="fas fa-plus"></i>
-                            </button>
-                          </div>
-                          <div className="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                            <h6 className="mb-0">€ 44.00</h6>
-                          </div>
-                          <div className="col-md-1 col-lg-1 col-xl-1 text-end">
-                            <a href="#!" className="text-muted">
-                              <i className="fas fa-times"></i>
-                            </a>
-                          </div>
-                        </div>
-
-                        <hr className="my-4" /> */}
+                        {cartItems.length ? (
+                          cartItems.map((item) => <CartCard item={item} />)
+                        ) : (
+                          <h5>Cart empty</h5>
+                        )}
 
                         <div className="pt-5">
                           <h6 className="mb-0">
-                            <a href="#!" className="text-body">
+                            <Link to="/" className="text-body">
                               <i className="fas fa-long-arrow-alt-left me-2"></i>
                               Back to shop
-                            </a>
+                            </Link>
                           </h6>
                         </div>
                       </div>
@@ -209,7 +76,10 @@ function CartPage() {
                               id="form3Examplea2"
                               className="form-control form-control-lg"
                             />
-                            <label className="form-label" for="form3Examplea2">
+                            <label
+                              className="form-label"
+                              htmlFor="form3Examplea2"
+                            >
                               Enter your code
                             </label>
                           </div>
