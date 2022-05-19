15aa7da954f6ce723c4277a89ea6d7ee629445f9
LloydRivers <lloydrivers58@hotmail.com>
2022-05-19 17:30:17 +0100

    removed container

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Container } from "react-bootstrap";
+
 import AllRoutes from "./config/routing/AllRoutes";
 
 export const CartContext = React.createContext();
 function App() {
   const [cartItems, setCartItems] = useState([]);
   return (
-    <Container>
-      <CartContext.Provider value={[cartItems, setCartItems]}>
-        <AllRoutes />
-      </CartContext.Provider>
-    </Container>
+    <CartContext.Provider value={[cartItems, setCartItems]}>
+      <AllRoutes />
+    </CartContext.Provider>
   );
 }
 
