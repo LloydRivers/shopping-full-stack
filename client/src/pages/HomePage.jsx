5c633f958df5eec00b59894cc90f3e33e401c8c2
LloydRivers <lloydrivers58@hotmail.com>
2022-05-20 14:24:15 +0100

    import app container

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Section from "../components/section/Section";
 import Hero from "./Hero";
 import axios from "axios";
+import { AppContainer } from "../components/app.styled/App.styled";
 
 function HomePage() {
   const [data, setData] = useState([]);
@@ -19,26 +20,30 @@ function HomePage() {
   return (
     <>
       <Hero />
-      <Section
-        data={data
-          .filter((obj) => obj.category === "men's clothing")
-          .slice(0, 4)}
-        title={"Men's Clothing"}
-      />
-      <Section
-        data={data.filter((obj) => obj.category === "jewelery").slice(0, 4)}
-        title={"Jewelery"}
-      />
-      <Section
-        data={data.filter((obj) => obj.category === "electronics").slice(0, 4)}
-        title={"Electronics"}
-      />
-      <Section
-        data={data
-          .filter((obj) => obj.category === "women's clothing")
-          .slice(0, 4)}
-        title={"Women's clothing"}
-      />
+      <AppContainer>
+        <Section
+          data={data
+            .filter((obj) => obj.category === "men's clothing")
+            .slice(0, 4)}
+          title={"Men's Clothing"}
+        />
+        <Section
+          data={data.filter((obj) => obj.category === "jewelery").slice(0, 4)}
+          title={"Jewelery"}
+        />
+        <Section
+          data={data
+            .filter((obj) => obj.category === "electronics")
+            .slice(0, 4)}
+          title={"Electronics"}
+        />
+        <Section
+          data={data
+            .filter((obj) => obj.category === "women's clothing")
+            .slice(0, 4)}
+          title={"Women's clothing"}
+        />
+      </AppContainer>
     </>
   );
 }
